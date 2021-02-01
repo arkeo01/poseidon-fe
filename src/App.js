a50605444d9d0e1499fcca7c9fe0d4333aff6803
arkeo01 <kishor.1@iitj.ac.in>
2021-02-02 01:15:39 +0530

    State object to store web3, web3Socket and their instances

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Web3 from 'web3';
+import lendingPoolAddressesProviderABI from './LendingPoolAddressProviderABI';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      web3: {},
+      web3Socket: {},
+      instance: {},
+      socketInstance: {}
+    }
+  }
+  
+  componentDidMount () {
+    if (window.ethereum != null) {
+      const web3 = new Web3(window.ethereum);      
+      if(web3){
+        this.setState({
+          web3: web3
+        });
+      }
+    }
+  }
+
+  getAccounts = async () => {
+    const accounts = await this.state.web3.eth.getAccounts();
+    console.log(accounts);
+  }
+
+  getWeb3Socket = async () => {
+    console.log('Inhere')
+    try{
+      let web3Socket = this.state.web3;
+      const networkName = await this.state.web3.eth.net.getNetworkType();
+      if(networkName === "kovan"){
+        web3Socket = new Web3(new Web3.providers.WebsocketProvider("wss://kovan.infura.io/ws/v3/fc9da9a67ca34268aaaf83eec25c91db"));
+        console.log(web3Socket)
+        this.setState({
+          web3Socket: web3Socket
+        })
+      }
+      console.log(this.state);
+    }
+    catch(err){
+      console.log(err);
+    }
+  };
+
+  loadAdressesProviderContract = async (address) => {
+    const instance = await new this.state.web3.eth.Contract(lendingPoolAddressesProviderABI, address);
+    const socketInstance = await new this.state.web3Socket.eth.Contract(lendingPoolAddressesProviderABI, address);
+    this.setState({
+      instance,
+      socketInstance
+    });
+
+    console.log(this.state);
+  }
+
+  // TODO: Complete Function for Subscribing to events using socket
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <button
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => this.getWeb3Socket()}
+          >
+            Fire Web3Socket
+          </button>
+          <button
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => this.loadAdressesProviderContract('0x88757f2f99175387aB4C6a4b3067c77A695b0349')}
+          >
+            Instantiate contracts
+          </button>
+        </header>
+      </div>
+    );
+  }
+
+};
 
 export default App;
